523aa1e013a2fd5e835f97f6f4d509d49aa97ffa
danny n <dnoest@gmail.com>
2019-01-24 20:17:26 -0600

    cross product

--- src/Math.hs
+++ src/Math.hs
@@ -1,4 +1,7 @@
-module Math (Vector(..), point, vec )  where
+module Math (Vector(..), point, vec, norm, dot, cross, mulScalar )  where
+
+-- For comparing Vectors. 
+epsilon = 0.00000001
 
 data Vector  = Vector { x :: Double
                       , y :: Double 
@@ -9,17 +12,31 @@ instance Num Vector where
     (Vector a b c d) + (Vector x y z w) = Vector (a+x) (b+y) (c+z) (d+w)
     (Vector a b c d) * (Vector x y z w) = Vector (a+x) (b+y) (c+z) (d+w)
     negate (Vector x y z w) = Vector (-x) (-y) (-z) (-w)
-    abs (Vector x y z w) =  1
+    abs (Vector x y z w) = 1
     fromInteger n = Vector (fromIntegral n) (fromIntegral n) (fromIntegral n) (fromIntegral n) 
 
+vec :: Double -> Double -> Double -> Vector
 vec x y z = Vector x y z 0
+
+point :: Double -> Double -> Double -> Vector
 point x y z = Vector x y z 1
 
-epsilon = 0.00000001
-
+relEq :: Double -> Double -> Bool
 relEq a b 
     | (abs (a - b)) <= epsilon = True
     | otherwise      = False
+
+mulScalar :: Vector -> Double -> Vector
+mulScalar (Vector x y z w) r = Vector (x*r) (y*r) (z*r) (w*r)
+
+norm :: Vector -> Double
+norm (Vector x y z _) = sqrt (x^2 + y^2 + z^2)
+
+dot :: Vector -> Vector -> Double
+dot (Vector a b c d) (Vector x y z w) = (a * x) + (b * y) + (c * z) + (d * w)
+
+cross :: Vector -> Vector -> Vector
+cross (Vector a b c d) (Vector x y z w) = vec (b*z - c*y)  (c*x -a*z) (a*y - b*x)
 
 instance Eq Vector where
     (Vector a b c d) == (Vector x y z w) 
--- test/Math/MathSpec.hs
+++ test/Math/MathSpec.hs
@@ -18,20 +18,36 @@ instance Arbitrary Vector where
 
 prop_Operations :: Vector -> Vector -> Bool
 prop_Operations u v = u + v == v - (-u)
+
+prop_Distributivity :: Vector -> Vector -> Double -> Bool
+prop_Distributivity u v r = mulScalar (u+v) r == (mulScalar u r) + (mulScalar v r)
  
 spec :: Spec
 spec = do
   describe "math" $ do
-    it "create a point" $ do
-        (point 4.3 (-4.2) 3.1) `shouldBe` (Vector 4.3 (-4.2) 3.1 1.0)
-    it "create a vec" $ do
-        (vec 4.3 (-4.2) 3.1) `shouldBe` (Vector 4.3 (-4.2) 3.1 0.0)
-    it "subtraction" $ do
-        let u = (point 3.0 2.0 1.0)
-        let v = (point 5.0 6.0 7.0)
-        (u - v) `shouldBe` (Vector (-2.0) (-4.0) (-6.0) 0.0)
-    it "addition" $ do
-        let u = (Vector 3.0 (-2.0) 5.0 1.0)
-        let v = (Vector (-2.0) 3.0 1.0 0.0)
-        (u + v) `shouldBe` (Vector 1.0 1.0 6.0 1.0)
-    it "operations" $ property $ prop_Operations
+    describe "vector" $ do
+        it "create a point" $ do
+            (point 4.3 (-4.2) 3.1) `shouldBe` (Vector 4.3 (-4.2) 3.1 1.0)
+        it "create a vec" $ do
+            (vec 4.3 (-4.2) 3.1) `shouldBe` (Vector 4.3 (-4.2) 3.1 0.0)
+        it "addition" $ do
+            let u = (Vector 3.0 (-2.0) 5.0 1.0)
+            let v = (Vector (-2.0) 3.0 1.0 0.0)
+            (u + v) `shouldBe` (Vector 1.0 1.0 6.0 1.0)
+        it "subtraction" $ do
+            let u = (point 3.0 2.0 1.0)
+            let v = (point 5.0 6.0 7.0)
+            (u - v) `shouldBe` (Vector (-2.0) (-4.0) (-6.0) 0.0)
+        it "norm" $ do 
+            let u = (vec 1.0 2.0 3.0)
+            (norm u) `shouldBe` (sqrt 14.0)
+        it "dot" $ do
+            let u = (vec 1.0 2.0 3.0)
+            let v = (vec 2.0 3.0 4.0)
+            (dot u v) `shouldBe` 20.0
+        it "cross" $ do
+            let u = (vec 1.0 2.0 3.0)
+            let v = (vec 2.0 3.0 4.0)
+            (cross u v) `shouldBe` (vec (-1.0) 2.0 (-1.0))
+        it "operations" $ property $ prop_Operations
+        it "distributivity" $ property $ prop_Distributivity
