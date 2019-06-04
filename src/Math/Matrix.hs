4e08a6e736ef77b4845790e5efe34a1b19478871
danny n <dnoest@gmail.com>
2019-06-04 16:06:36 -0400

    matrix multiplication works

--- src/Math/Matrix.hs
+++ src/Math/Matrix.hs
@@ -2,33 +2,85 @@
 
 module Math.Matrix
 ( Matrix(..)
+, identity
+, matrixFromList
+, getEntry
+, matrixByScaler 
 ) where
 
+import Math
 import Math.Vector
 
 import qualified Data.Vector as V
 
-data Matrix = Matrix (V.Vector Double)
+data Matrix = Matrix (V.Vector Double) deriving (Show)
 
-identityMatrix :: Matrix
-identityMatrix = Matrix $ V.fromList [ 1.0, 0.0, 0.0, 0.0
-                 , 0.0, 1.0, 0.0, 0.0
-                 , 0.0, 0.0, 1.0, 0.0
-                 , 0.0, 0.0, 0.0, 1.0 ]
+identity :: Matrix
+identity = Matrix $ V.fromList [ 1.0, 0.0, 0.0, 0.0
+                               , 0.0, 1.0, 0.0, 0.0
+                               , 0.0, 0.0, 1.0, 0.0
+                               , 0.0, 0.0, 0.0, 1.0 ]
+
+matrixFromList :: [Double] -> Matrix
+matrixFromList l = Matrix $ V.fromList l
 
 translation :: Vector -> Matrix
-translation v = identityMatrix
+translation (Vector x y z _) = matrixFromList [ 1.0, 0.0, 0.0, x
+                                              , 0.0, 1.0, 0.0, y
+                                              , 0.0, 0.0, 1.0, z
+                                              , 0.0, 0.0, 0.0, 1.0 ]
 
 scaling :: Vector -> Matrix
-scaling v = identityMatrix
+scaling (Vector x y z _) = matrixFromList [   x, 0.0, 0.0, 0.0
+                                          , 0.0,   y, 0.0, 0.0
+                                          , 0.0, 0.0,   z, 0.0
+                                          , 0.0, 0.0, 0.0, 1.0 ]
 
-rotation :: Vector -> Matrix
-rotation v = identityMatrix
+getEntry :: Matrix -> Int -> Int -> Double
+getEntry (Matrix m) x y = m V.!( (y * 4) + x)
 
---instance Num Vector where
---    (Vector a b c d) + (Vector x y z w) = Vector (a+x) (b+y) (c+z) (d+w)
---    (Vector a b c d) * (Vector x y z w) = Vector (a+x) (b+y) (c+z) (d+w)
+instance Eq Matrix where
+    (Matrix a) == (Matrix b)
+        |  (relEq (a V.!0) (b V.!0)) && (relEq (a V.!1) (b V.!1)) && (relEq (a V.!2) (b V.!2)) && (relEq (a V.!3) (b V.!3)) 
+        && (relEq (a V.!4) (b V.!4)) && (relEq (a V.!5) (b V.!5)) && (relEq (a V.!6) (b V.!6)) && (relEq (a V.!7) (b V.!7))
+        && (relEq (a V.!8) (b V.!8)) && (relEq (a V.!9) (b V.!9)) && (relEq (a V.!10) (b V.!10)) && (relEq (a V.!11) (b V.!11)) 
+        && (relEq (a V.!12) (b V.!12)) && (relEq (a V.!13) (b V.!13)) && (relEq (a V.!14) (b V.!14)) && (relEq (a V.!15) (b V.!15)) = True
+        | otherwise = False
+
+matrixMulEntry :: Matrix -> Matrix -> Int -> Int -> Double
+matrixMulEntry m1 m2 x y = (a0 * b0) + (a1 * b1) + (a2 * b2) + (a3 * b3)
+    where a0 = getEntry m1 0 y
+          a1 = getEntry m1 1 y
+          a2 = getEntry m1 2 y
+          a3 = getEntry m1 3 y 
+          b0 = getEntry m2 x 0
+          b1 = getEntry m2 x 1
+          b2 = getEntry m2 x 2
+          b3 = getEntry m2 x 3
+
+
+instance Num Matrix where
+    (Matrix a) + (Matrix b) = identity
+    a * b = matrixFromList [ matrixMulEntry a b 0 0
+                           , matrixMulEntry a b 1 0 
+                           , matrixMulEntry a b 2 0 
+                           , matrixMulEntry a b 3 0 
+                           , matrixMulEntry a b 0 1 
+                           , matrixMulEntry a b 1 1 
+                           , matrixMulEntry a b 2 1 
+                           , matrixMulEntry a b 3 1 
+                           , matrixMulEntry a b 0 2 
+                           , matrixMulEntry a b 1 2 
+                           , matrixMulEntry a b 2 2 
+                           , matrixMulEntry a b 3 2 
+                           , matrixMulEntry a b 0 3 
+                           , matrixMulEntry a b 1 3 
+                           , matrixMulEntry a b 2 3 
+                           , matrixMulEntry a b 3 3 ] 
 --    negate (Vector x y z w) = Vector (-x) (-y) (-z) (-w)
 --    abs (Vector x y z w) = 1
 --    fromInteger n = Vector (fromIntegral n) (fromIntegral n) (fromIntegral n) (fromIntegral n) 
+
+matrixByScaler :: Matrix -> Double -> Matrix
+matrixByScaler (Matrix m) d = Matrix (V.map (* d) m)
 
--- test/Math/MatrixSpec.hs
+++ test/Math/MatrixSpec.hs
@@ -3,6 +3,8 @@ module Math.MatrixSpec (main, spec) where
 import Test.Hspec
 import Test.QuickCheck
 
+import qualified Data.Vector as V
+
 import Math
 import Math.Vector 
 import Math.Matrix
@@ -10,10 +12,28 @@ import Math.Matrix
 main :: IO ()
 main = hspec spec
 
-
 spec :: Spec
 spec = do
   describe "math" $ do
-    describe "vector" $ do
-        it "create a point" $ do
-            True `shouldBe` True
+    describe "matrix" $ do
+        it "equality" $ do
+            let m1 = matrixFromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2]
+            let m2 = matrixFromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2]
+            m1 `shouldBe` m2 
+        it "inequality" $ do
+            let m1 = matrixFromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2]
+            let m2 = matrixFromList [2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+            m1 `shouldNotBe` m2 
+        it "get entry" $ do
+            let m = matrixFromList [1, 2, 3, 4, 5, 6, 7, 8, 8, 9, 7, 6, 5, 4, 3, 2]
+            (getEntry m 1 2) `shouldBe` 9.0
+        it "dont get entry" $ do
+            let m = identity
+            (getEntry m 1 1) `shouldNotBe` (getEntry m 3 2)
+        it "matrix multiplication" $ do
+            let m1 = matrixFromList [1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2]
+            let m2 = matrixFromList [(-2), 1, 2, 3, 3, 2, 1, (-1), 4, 3, 6, 5, 1, 2, 7, 8]
+            let m3 = matrixFromList [20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42]
+            (m1 * m2) `shouldBe` m3
+
+
